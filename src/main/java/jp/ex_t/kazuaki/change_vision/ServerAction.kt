aa458b94662a243651eb772a2a689d730642f520
HyodaKazuaki <kazuaki@ex-t.jp>
2021-03-16 18:39:07 +0900

    Add server feature

---
+++ src/main/java/jp/ex_t/kazuaki/change_vision/ServerAction.kt
@@ -0,0 +1,40 @@
+package jp.ex_t.kazuaki.change_vision
+
+import com.change_vision.jude.api.inf.ui.IPluginActionDelegate
+import com.change_vision.jude.api.inf.ui.IWindow
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import javax.swing.JOptionPane
+
+class ServerAction: IPluginActionDelegate {
+    private var isLaunchedServer = false
+    private lateinit var socketServer: SocketServer
+    override fun run(window: IWindow) {
+        try {
+            if (!isLaunchedServer) {
+                val portNumber = JOptionPane.showInputDialog(window.parent, "Input server port.").toInt()
+                if ((0 > portNumber) or (65535 < portNumber)) throw NumberFormatException()
+                socketServer = SocketServer(portNumber)
+                socketServer.launch()
+                GlobalScope.launch {
+                    kotlin.runCatching {
+                        withContext(Dispatchers.IO) {
+                            socketServer.serve()
+                        }
+                    }
+                }
+            } else {
+                socketServer.stop()
+            }
+            isLaunchedServer = !isLaunchedServer
+
+        } catch (e: NumberFormatException) {
+            JOptionPane.showMessageDialog(window.parent, "Port number must be an integer and be 0 - 65535.", "Warning", JOptionPane.WARNING_MESSAGE)
+        } catch (e: Exception) {
+            JOptionPane.showMessageDialog(window.parent, "Exception occurred.", "Alert", JOptionPane.ERROR_MESSAGE)
+            throw e
+        }
+    }
+}
---
+++ src/main/java/jp/ex_t/kazuaki/change_vision/SocketServer.kt
@@ -0,0 +1,51 @@
+package jp.ex_t.kazuaki.change_vision
+
+import java.io.*
+import java.net.InetSocketAddress
+import java.net.ServerSocket
+
+class SocketServer(private val portNumber: Int) {
+    private lateinit var serverSocket: ServerSocket
+    private var isLaunched = false
+    fun launch() {
+        serverSocket = ServerSocket()
+        serverSocket.reuseAddress = true
+        serverSocket.bind(InetSocketAddress(portNumber))
+        isLaunched = true
+        println("Launched server.")
+    }
+
+    fun stop() {
+        isLaunched = false
+        println("Stopping server...")
+    }
+
+    fun serve() {
+        while (isLaunched) {
+            val socket = serverSocket.accept()
+            val inputStream = socket.getInputStream()
+            val bufferedReader = BufferedReader(InputStreamReader(inputStream))
+            val outputStream = socket.getOutputStream()
+            val printWriter = PrintWriter(BufferedWriter(OutputStreamWriter(outputStream)))
+            while (true) {
+                while (inputStream.available() == 0) {}
+                val receivedMessage = bufferedReader.readLine()
+                if (receivedMessage.first() == '\n') {
+                    break
+                }
+                println("Received: ${receivedMessage}")
+                printWriter.write(receivedMessage)
+                printWriter.flush()
+            }
+            bufferedReader.close()
+            printWriter.close()
+            socket.close()
+        }
+        close()
+    }
+
+    private fun close() {
+        serverSocket.close()
+        println("Stopped server.")
+    }
+}
